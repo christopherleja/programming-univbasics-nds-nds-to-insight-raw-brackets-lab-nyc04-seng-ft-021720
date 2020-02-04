65408adea68744bf808dc2e9b326112dfe7d0281
Christopher Leja <christopher.leja@gmail.com>
2020-02-04 22:32:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,23 +4,16 @@ require 'directors_database'
 def directors_totals(nds)
  director_name = []
 total_gross = {}
- 
-
 row_index = 0
-# puts directors_database
+
 while row_index < directors_database.length do
   director_name.push("#{directors_database[row_index][:name]}")
   column_index = 0
-  # puts "row_index = #{row_index}, directors_database.length = #{directors_database.length}"
+  
   value_total = 0 
   while column_index < directors_database[row_index][:movies].length do
- # puts "column_index = #{column_index}"
-  #puts "row index = #{row_index}"
-  
     
         value_total += directors_database[row_index][:movies][column_index][:worldwide_gross]
-        
-       # puts "director #{director_name[row_index]}, value #{value_total}"
    
     column_index += 1
   end
